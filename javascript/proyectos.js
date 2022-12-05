2a2ce346a1fad5e1f892fe7fbf7b2f50c8ed39ab
Matias <matute_11@hotmail.com.ar>
2022-12-05 01:41:49 -0300

    Agrego seccion Proyectos

---
+++ javascript/proyectos.js
@@ -0,0 +1,13 @@
+const boton = document.getElementsByClassName("btn__proyectos");
+
+function cambiarPaginaCafe(){
+    document.location.href = "https://cafeblogmatias.netlify.app/";
+}
+
+function cambiarPaginaStore(){
+    document.location.href = "https://tiendatessera04.netlify.app/"
+}
+
+function cambiarPaginaPasteleria(){
+    document.location.href = "https://pasteleriaana.netlify.app/"
+}
